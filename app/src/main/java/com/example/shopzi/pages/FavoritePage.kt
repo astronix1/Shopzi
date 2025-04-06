ce2d4ec6dba027465ece7abd4416a89800d4a8ce
astronix1 <astronixvishal@gmail.com>
2025-04-06 15:43:39 +0530

    implemented bottom navigation bar functionality

---
+++ app/src/main/java/com/example/shopzi/pages/FavoritePage.kt
@@ -0,0 +1,9 @@
+package com.example.shopzi.pages
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FavoritePage(modifier: Modifier = Modifier) {
+    
+}
