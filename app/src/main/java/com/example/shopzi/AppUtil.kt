8561a084e7b37661573a5fb68fe6fd29df1af0d1
astronix1 <astronixvishal@gmail.com>
2025-04-19 17:01:39 +0530

    Added add to cart functionality in app util

--- app/src/main/java/com/example/shopzi/AppUtil.kt
+++ app/src/main/java/com/example/shopzi/AppUtil.kt
@@ -2,9 +2,30 @@ package com.example.shopzi
 
 import android.content.Context
 import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
 
 object AppUtil {
     fun showToast(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+    fun addtocart(context: Context, productId: String){
+        val userdoc = Firebase.firestore.collection("users")
+            .document(Firebase.auth.currentUser?.uid!!)
+        userdoc.get().addOnCompleteListener {
+            if(it.isSuccessful){
+                val cart = it.result.get("cart") as? Map<String, Long>?:emptyMap()
+                val cq = cart[productId]?:0
+                val uc = mapOf("cart.$productId" to cq+1)
+                userdoc.update(uc).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        showToast(context, "Added to cart")
+                    }else{
+                        showToast(context, "Failed to add to cart")
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/shopzi/model/UserModel.kt
+++ app/src/main/java/com/example/shopzi/model/UserModel.kt
@@ -1,7 +1,8 @@
 package com.example.shopzi.model
 
 data class UserModel(
-    val name: String,
-    val email: String,
-    val uid: String
+    val name: String="",
+    val email: String="",
+    val uid: String="",
+    val cart: Map<String, Long> = emptyMap()
 )
